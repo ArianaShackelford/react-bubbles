87f1e5ecbca396eb24d352e3743a74a504f58a51
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-20 12:50:21 -0500

    rearranged routing and added a switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -10,8 +10,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Switch>
         <PrivateRoute path='/protected' component={BubblePage}/>
+        <Route path="/login" component={Login} />
+        <Route component={Login}/>
+          
+        </Switch>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
