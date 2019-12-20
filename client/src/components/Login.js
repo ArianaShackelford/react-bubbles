76091857504e2c86ce5ce905712f974d37af38ab
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-20 12:49:47 -0500

    login finished

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,25 +4,27 @@ import {axiosWithAuth} from '../utils/axiosWithAuth';
 const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [credentials, setCredentials] = useState({});
-
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
   const login = e => {
     e.preventDefault();
+    console.log(credentials);
     axiosWithAuth().post('/login', credentials)
       .then(res => {
         console.log(res);
-        // localStorage.setItem('token', res.data.token);
-        // this.props.history.push('/')
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/protected')
       })
       .catch(err => console.log(err))
   };
 
   const handleChange = event => {
     setCredentials({
-        credentials: {
-        ...credentials,
+         ...credentials,
         [event.target.name]: event.target.value
-        }
+       
     });
 };
 
